659c2d9e690ff42f8773df2cd4bacac44f5872ee
Linda Nichols <lynnaloo@gmail.com>
2017-06-11 18:44:48 -0400

    [fix] remove extra event listeners

--- ibm/raspberry-pi/index.js
+++ ibm/raspberry-pi/index.js
@@ -3,22 +3,15 @@ const moment = require('moment-timezone');
 const Io = require('raspi-io');
 const RaspiCam = require('raspicam');
 const fs = require('fs');
-//const path = require('path');
-//const ObjectStorage = require('bluemix-objectstorage').ObjectStorage;
+const path = require('path');
 const Client = require('ibmiotf');
-
-// const storageConfig = {
-//   projectId: process.env.STORAGE_PROJECT_ID,
-//   userId: process.env.STORAGE_USER_ID,
-//   password: process.env.STORAGE_PASSWORD,
-//   region: ObjectStorage.Region.DALLAS
-// };
-//const objStorage = new ObjectStorage(storageConfig);
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
 const iotConfig = {
-  'org': process.env.IOT_ORG, // o1qhwe
-  'id': process.env.IOT_DEVICE_ID, // raspberry-kitty
-  'type': process.env.IOT_DEVICE_TYPE, // raspberry-pi
+  'org': process.env.IOT_ORG,
+  'id': process.env.IOT_DEVICE_ID,
+  'type': process.env.IOT_DEVICE_TYPE,
   'domain': 'internetofthings.ibmcloud.com',
   'auth-method': 'token',
   'auth-token': process.env.IOT_AUTH_TOKEN
@@ -32,6 +25,7 @@ const camera = new RaspiCam({
   encoding: 'jpg',
 	timeout: 0 // take the picture immediately
 });
+
 const board = new five.Board({
   io: new Io()
 });
@@ -42,44 +36,12 @@ board.on('ready', () => {
     pin: 'P1-7', //PIR is wired to pin 7 (GPIO 4)
     freq: 100
   });
+
   // This happens once at the begnning of the session. The default state.
   motion.on('calibrated', () => {
     console.log('Motion detector calibrated and ready');
   });
 
-  // setting the log level
-  device.log.setLevel('info');
-  // log errors
-  device.on('error', err => {
-    console.log(`Error: ${err.code} while connecting to ${err.hostname}`);
-  });
-
-  device.on('connect', () => {
-    console.log('Connected to IBM Iot');
-
-    motion.on('motionstart', data => {
-      const now = moment().tz('America/New_York').format('LLL');
-      console.log(`Motion Alert: something was spotted at: ${now}`);
-      if (camera) {
-        // take a photo
-        startCamera();
-      } else {
-        console.log('Oops. There is no camera connected.');
-      }
-    });
-
-    motion.on('motionend', () => {
-      console.log('Motion has stopped for 100ms');
-    });
-  });
-
-  // connect to IBM IoT
-  console.log('Connecting to IBM IoT Platform');
-  device.connect();
-});
-
-function startCamera() {
-  camera.start(); // take a picture
   camera.on('start', (err, timestamp) => {
     console.log('Camera is taking a photo!');
   });
@@ -95,36 +57,69 @@ function startCamera() {
         console.log('Problem reading file', err);
         throw err;
       }
+      const timestamp = moment().valueOf();
+      const newFileName = `${path.parse(filename).name}-${timestamp}${path.parse(filename).ext}`;
 
-      // const timestamp = moment().valueOf();
-      // const newFileName = `${path.parse(filename).name}-${timestamp}${path.parse(filename).ext}`;
+      const params = {
+        Bucket: 'kitty-detections',
+        Key: newFileName,
+        Body: data, // file buffer
+        ContentType: 'image/jpeg',
+        ACL: 'public-read' // this is temporary fix
+      };
 
-      // const params = {
-      //   Key: newFileName,
-      //   Body: data, // file buffer
-      //   ContentType: 'image/jpeg'
-      // };
-
-      // uploadFile(params)
-      // .then((imageUrl) => {
+      uploadFile(params)
+      .then((imageUrl) => {
         const detectionObj = {
           'motion': true,
           'timestamp': moment().tz('America/New_York').format('LLL'),
-          'url': 'https://raw.githubusercontent.com/lynnaloo/mowgli.ninja/master/mowgli-1.JPG'
+          'url': imageUrl
         };
         console.log('publishing event', JSON.stringify(detectionObj));
         device.publish('kitty-detection', 'json', JSON.stringify(detectionObj));
-      // });
+      });
+    });
+  });
+
+  device.on('connect', () => {
+    console.log('Connected to IBM Watson IoT Platform');
+    motion.on('motionstart', data => {
+      const now = moment().tz('America/New_York').format('LLL');
+      console.log(`Motion Alert: something was spotted at: ${now}`);
+      if (camera) {
+        // take a photo
+        camera.start(); // take a picture
+      } else {
+        console.log('Oops. There is no camera connected.');
+      }
+    });
+
+    motion.on('data', (data) => {
+      console.log('data', data);
+    });
+
+    motion.on('motionend', () => {
+      console.log('Motion has stopped for 100ms');
+    });
+  });
+
+  device.on('error', err => {
+    console.log(`Error: ${err.code} while connecting to ${err.hostname}`);
+  });
+  device.connect();
+});
+
+// TODO: move this to a separate library
+function uploadFile(params) {
+  return new Promise((resolve, reject) => {
+    s3.putObject(params, (err, data) => {
+      if (err) {
+        console.log('Problem uploading image', err);
+        reject(new Error(err));
+      }
+      const imageUrl = `https://s3.amazonaws.com/${params.Bucket}/${params.Key}`;
+      console.log('Image successfully uploaded', imageUrl);
+      resolve(imageUrl);
     });
   });
 }
-
-// TODO: move this to a separate library
-// function uploadFile(params) {
-//   return new Promise((resolve, reject) => {
-//     console.log('Image successfully uploaded');
-//     const imageUrl = `https://s3.amazonaws.com/${params.Bucket}/${params.Key}`;
-//     resolve(imageUrl);
-//   });
-// }
-
